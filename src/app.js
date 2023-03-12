919871329524f2ee2bb8d2392144068ba1521233
Toni369-vs <tonicentenera@gmail.com>
2023-03-12 15:02:35 +0000

    GeneradorDeExcusasFinalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const resultado = document.getElementById("excusa");
+const limpiarBoton = document.getElementById("clean");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generadorDeExcusas() {
+  let who = ["El jefe", "Mi compañero", "El Formador"];
+  let what = [
+    "me da pereza",
+    "es muy cansino",
+    "no lo soporto",
+    "es un inútil",
+    "no hace nada",
+    "es un pelele"
+  ];
+  let when = [
+    "siempre que lo veo",
+    "día sí y día también ",
+    "hoy y mañana",
+    "bocu de mucho",
+    "forever"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  let nuevaExcusa =
+    who[randomWho] + " " + what[randomWhat] + " " + when[randomWhen];
+  resultado.innerHTML = nuevaExcusa;
+}
+
+function limpiadorDeExcusas() {
+  document.getElementById("excusa").innerHTML = "";
+}
